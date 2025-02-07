8de903150bebfd9703598a8cf3dfc3c4da5a6af7
Jssoff <js.garay@uniandes.com>
2025-02-07 10:34:37 -0500

    settings

--- monitoring/settings.py
+++ monitoring/settings.py
@@ -78,11 +78,11 @@ WSGI_APPLICATION = 'monitoring.wsgi.application'
 DATABASES = {
      "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "<nombrebd>",
-        "USER": "<usuario>",
-        "PASSWORD": "<clave>",
+        "NAME": "base",
+        "USER": "jess",
+        "PASSWORD": "SOFIA077",
         "HOST": "localhost",
-        "PORT": "",
+        "PORT": "5432",
     }
 }
 
